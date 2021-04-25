d2733165276a5e39f537b2417abcb4a5860b9e42
Fuyan Yuan <33221728+TheRainstorm@users.noreply.github.com>
2021-04-26 00:15:01 +0800

    fix coremark output %lu and specific Iteration num
    之后可以使用ee_printf作为ucore printf的实现

--- user/app/coremark/core_main.c
+++ user/app/coremark/core_main.c
@@ -355,8 +355,8 @@ for (i = 0; i < MULTITHREAD; i++)
     }
     total_errors += check_data_types();
     /* and report results */
-    ee_printf("CoreMark Size    : %lu\n", (long unsigned)results[0].size);
-    ee_printf("Total ticks      : %lu\n", (long unsigned)total_time);
+    ee_printf("CoreMark Size    : %d\n", (long unsigned)results[0].size);
+    ee_printf("Total ticks      : %d\n", (long unsigned)total_time);
 #if HAS_FLOAT
     ee_printf("Total time (secs): %f\n", time_in_secs(total_time));
     if (time_in_secs(total_time) > 0)
@@ -377,7 +377,7 @@ for (i = 0; i < MULTITHREAD; i++)
         total_errors++;
     }
 
-    ee_printf("Iterations       : %lu\n",
+    ee_printf("Iterations       : %d\n",
               (long unsigned)default_num_contexts * results[0].iterations);
     ee_printf("Compiler version : %s\n", COMPILER_VERSION);
     ee_printf("Compiler flags   : %s\n", COMPILER_FLAGS);
